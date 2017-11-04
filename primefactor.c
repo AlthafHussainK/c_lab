54fb01244225ec71f211c96d4ba24696abcdf167
Althaf Hussain <althafakm@gmail.com>
2017-11-04 07:17:46 +0000

    update

--- primefactor.c
+++ primefactor.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 
-
 int main (void)
 {
  long long current = 600851475143; 
  int i = 3; // start at 3 for primes... we can do this since subject is ODD
  int high = 0;
-
 
  while (1) //infinite loop till hit break
  {
@@ -18,11 +16,9 @@ int main (void)
   
   if (current == 1)
    break;
-
-
+  
   i +=2 ; // skip all even numbers since not prime
  }
-
 
  printf ("Highest prime factor of 600851475143 is %d\n", high);
  return 1;
