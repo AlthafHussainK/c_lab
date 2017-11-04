962fc7552c0719f98aa188d9ea0811faf4e38a91
Althaf Hussain <althafakm@gmail.com>
2017-11-04 07:15:11 +0000

    update

--- countarray.c
+++ countarray.c
@@ -7,22 +7,20 @@ main()
 
 	nwhite = nother = 0;
 
-		for(i=0; i<10; ++i)
+		for(i = 0; i < 10; ++i)
 			ndigit[i] = 0;
 
-	while ((c=getchar()) != EOF)
+	while ((c = getchar()) != EOF)
 		if (c >= '0' && c <= '9')
-			++ndigit[c-'0'];
+			++ndigit[c - '0'];
 		else if (c == ' ' || c == '\n' || c == '\t')
 			++nwhite;
 		else
 			++nother;
 printf("digits = ");
 
-	for(i=0; i<10; ++i)
+	for(i = 0; i < 10; ++i)
 		printf("%d", ndigit[i]);
 printf(", White space = %d, other = %d\n", nwhite,nother);
-
-
 
 }
