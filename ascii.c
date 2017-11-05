e4fbba013a072fd24ba5b945df68104f17f26d54
Althaf Hussain <althafakm@gmail.com>
2017-11-05 21:55:55 +0100

    update

--- ascii.c
+++ ascii.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
+
 int main(void)
 {
 
-for(char i='A'; i< 'Z'; i++)
-{
-printf("%c is %i\n", (int) i, i);
-}
+  for(char i='A'; i< 'Z'; i++)
+  {
+    printf("%c is %i\n", (int) i, i);
+  }
 }
 
